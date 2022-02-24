39c1f83c95ae27e0c342a58dc37608071f5a3eca
Keshav Garg <keshav.garg@sentieo.com>
2022-02-24 16:28:25 +0530

    updated makefile

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -1,20 +1,2 @@
 import pytest
 
-from {{ cookiecutter.project_slug }}.cli.main import {{ cookiecutter.cement_app_name }}Test
-
-
-@pytest.mark.no_cover
-def test_{{ cookiecutter.project_slug }}():
-    # test {{ cookiecutter.project_slug }} without any subcommands or arguments
-    with {{ cookiecutter.cement_app_name }}Test() as app:
-        app.run()
-        assert app.exit_code == 0
-
-
-@pytest.mark.no_cover
-def test_{{ cookiecutter.project_slug }}_debug():
-    # test that debug mode is functional
-    argv = ["--debug"]
-    with {{ cookiecutter.cement_app_name }}Test(argv=argv) as app:
-        app.run()
-        assert app.debug is True
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app/py/server.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+import grpc
+from concurrent import futures
+
+PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
+ROOT_PATH = os.path.abspath(os.path.join('.', os.pardir))
+
+paths = [ROOT_PATH, PROJECT_ROOT]
+
+for path in paths:
+    if path not in sys.path:
+        sys.path.append(path)
+
+from sentieo.proto.{{cookiecutter.team}}.{cookiecutter.name}}.v1 import helloworld_pb2, helloworld_pb2_grpc
+from {{cookiecutter.project_slug}}.app.py.services.{{cookiecutter.name}}.service import NotesServicer
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    notes_pb2_grpc.add_HelloworldServicer_to_server(HelloworldServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
